fda9a3d9a2341c36443acfe7cf03995b15be7e88
Adam Zamozniewicz <adam.zamozniewicz@netcentric.biz>
2022-01-10 12:45:39 +0100

    Install MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import {
+  List, ListItem, ListItemIcon, ListItemText, Typography,
+} from '@mui/material';
+import FolderIcon from '@mui/icons-material/Folder';
 import './App.css';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <Typography variant="h6" component="h1" gutterBottom>AEM Explorer</Typography>
+      <List dense>
+        <ListItem>
+          <ListItemIcon>
+            <FolderIcon />
+          </ListItemIcon>
+          <ListItemText primary="First item" />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon>
+            <FolderIcon />
+          </ListItemIcon>
+          <ListItemText primary="Second item" />
+        </ListItem>
+      </List>
+    </div>
   );
 }
 
