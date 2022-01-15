5f5b0ef7125f47680da7c5d570db4f603580ac35
Adam Zamozniewicz <adam.zamozniewicz@netcentric.biz>
2022-01-15 21:09:26 +0100

    Move logo to header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { AppBar, Stack, Toolbar, Typography } from "@mui/material";
+
+export const Header: FC = () => {
+  return (
+    <AppBar position="fixed" color="primary">
+      <Toolbar>
+        <Stack direction="row" alignItems="center" spacing={1.5}>
+          <img src="icons/icon-128.png" alt="" width="20" height="20" />
+          <Typography variant="body1">AEM Explorer</Typography>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
+};
