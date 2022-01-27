1b3f9afb86b977a71023be8d539f523b93812098
Adam Zamozniewicz <adam.zamozniewicz@netcentric.biz>
2022-01-27 13:02:15 +0100

    Add context menu for switching between WCM modes

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,9 +1,44 @@
+import { WcmMode } from "../model/modes";
+import { includes } from "../utils/includes";
+import { setWcmMode } from "../utils/set-wcm-mode";
+
+const ids = [
+  "aemExplorerDisabled",
+  "aemExplorerEdit",
+  "aemExplorerPreview",
+  "aemExplorerDesign",
+] as const;
+
+type Id = typeof ids[number];
+
+const modes: Record<Id, WcmMode> = {
+  aemExplorerDisabled: "disabled",
+  aemExplorerEdit: "touch",
+  aemExplorerPreview: "preview",
+  aemExplorerDesign: "design",
+} as const;
+
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: "aemExplorer",
-    title: "AEM Explorer",
-    contexts: ["page"],
+  const contextMenus: Array<{ id: Id; title: string }> = [
+    { id: "aemExplorerDisabled", title: "WCM disabled" },
+    { id: "aemExplorerEdit", title: "Edit" },
+    { id: "aemExplorerPreview", title: "Preview" },
+    { id: "aemExplorerDesign", title: "Design" },
+  ];
+
+  contextMenus.forEach(({ id, title }) => {
+    chrome.contextMenus.create({
+      id,
+      title,
+      contexts: ["page"],
+    });
   });
+});
+
+chrome.contextMenus.onClicked.addListener(({ menuItemId }) => {
+  if (includes(ids, menuItemId)) {
+    setWcmMode(modes[menuItemId]);
+  }
 });
 
 export {};
--- src/utils/is-mode.ts
+++
@@ -1,5 +0,0 @@
-import { WcmMode, wcmModes } from "../model/modes";
-
-export const isMode = (mode: string): mode is WcmMode => {
-  return wcmModes.includes(mode as WcmMode);
-};
