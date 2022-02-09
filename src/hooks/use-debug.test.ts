ec7ff9a3433051dd51a3b195e73c7c8e0c9c52a1
Adam Zamozniewicz <adam.zamozniewicz@netcentric.biz>
2022-02-09 20:41:06 +0100

    Test hooks

---
+++ src/hooks/use-debug.test.ts
@@ -0,0 +1,41 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import { debugClientLibs } from "../utils/debug-client-libs";
+import { isDebuggingClientLibs } from "../utils/is-debugging-client-libs";
+import { useDebug } from "./use-debug";
+
+jest.mock("../utils/debug-client-libs", () => ({
+  debugClientLibs: jest.fn(),
+}));
+jest.mock("../utils/is-debugging-client-libs", () => ({
+  isDebuggingClientLibs: jest.fn(),
+}));
+jest.mock("../contexts/open-in-new-tab-context", () => ({
+  useOpenInNewTabContext: () => ({ openInNewTab: false }),
+}));
+
+describe("useDebug hook", () => {
+  it("allows you to switch debug mode", async () => {
+    (isDebuggingClientLibs as jest.Mock).mockResolvedValueOnce(true);
+    const { result, waitForNextUpdate } = renderHook(() => useDebug());
+    await waitForNextUpdate();
+
+    expect(result.current.debug).toBe(true);
+    expect(debugClientLibs).not.toHaveBeenCalled();
+
+    act(() => {
+      result.current.handleDebugChange(false);
+    });
+
+    expect(result.current.debug).toBe(false);
+    expect(debugClientLibs).toHaveBeenCalledTimes(1);
+    expect(debugClientLibs).toHaveBeenCalledWith(false, false);
+
+    act(() => {
+      result.current.handleDebugChange(true);
+    });
+
+    expect(result.current.debug).toBe(true);
+    expect(debugClientLibs).toHaveBeenCalledTimes(2);
+    expect(debugClientLibs).toHaveBeenCalledWith(true, false);
+  });
+});
