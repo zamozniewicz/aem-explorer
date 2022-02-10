0bfa486e6b13085b2c56324d3215b37cf7774587
Adam Zamozniewicz <adam.zamozniewicz@netcentric.biz>
2022-02-10 08:52:46 +0100

    Move context menus consts to a model

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,31 +1,8 @@
-import { WcmMode } from "../model/modes";
+import { contextMenus, ids, modes } from "../model/context-menus";
 import { includes } from "../utils/includes";
 import { setWcmMode } from "../utils/set-wcm-mode";
 
-const ids = [
-  "aemExplorerDisabled",
-  "aemExplorerEdit",
-  "aemExplorerPreview",
-  "aemExplorerDesign",
-] as const;
-
-type Id = typeof ids[number];
-
-const modes: Record<Id, WcmMode> = {
-  aemExplorerDisabled: "disabled",
-  aemExplorerEdit: "touch",
-  aemExplorerPreview: "preview",
-  aemExplorerDesign: "design",
-} as const;
-
 chrome.runtime.onInstalled.addListener(() => {
-  const contextMenus: Array<{ id: Id; title: string }> = [
-    { id: "aemExplorerDisabled", title: "WCM disabled" },
-    { id: "aemExplorerEdit", title: "Edit" },
-    { id: "aemExplorerPreview", title: "Preview" },
-    { id: "aemExplorerDesign", title: "Design" },
-  ];
-
   contextMenus.forEach(({ id, title }) => {
     chrome.contextMenus.create({
       id,
