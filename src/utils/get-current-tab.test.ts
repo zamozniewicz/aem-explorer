f6c51bf334c393af838cdf3e979a7e5856b119a8
Adam Zamozniewicz <adam.zamozniewicz@netcentric.biz>
2022-02-15 11:43:55 +0100

    Test browser environment fallback

--- src/utils/get-current-tab.test.ts
+++ src/utils/get-current-tab.test.ts
@@ -5,6 +5,17 @@ import { getCurrentTab } from "./get-current-tab";
 const mockedChromeTabsQuery = asMock(chrome.tabs.query);
 
 describe("getCurrentTab helper", () => {
+  const { location } = window;
+
+  beforeAll(() => {
+    delete (window as any).location;
+    (window as any).location = { href: "http://example.com" };
+  });
+
+  afterAll(() => {
+    window.location = location;
+  });
+
   it("resolves with the current tab", async () => {
     mockedChromeTabsQuery.mockImplementationOnce(() =>
       Promise.resolve([mockTab()])
@@ -12,5 +23,12 @@ describe("getCurrentTab helper", () => {
     await getCurrentTab();
 
     expect(chrome.tabs.query).toHaveBeenCalled();
+  });
+
+  it("works in a browser environment", async () => {
+    (chrome.tabs as unknown) = undefined;
+
+    const tab = await getCurrentTab();
+    expect(tab.url).toBe("http://example.com");
   });
 });
